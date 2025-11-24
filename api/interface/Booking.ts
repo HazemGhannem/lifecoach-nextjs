d10d7eb2bb96ff28c1b9621c0665710e086026dd
HazemGhannem <ghannemhazem@gmail.com>
2025-11-24 10:01:00 +0100

    t

--- api/interface/Booking.ts
+++
@@ -1,9 +0,0 @@
-export interface CreateBookingInput {
-  name?: string;
-  email?: string;
-  phone?: string;
-  date?: string;
-  time?: string;
-  message?: string;
-  package?: "single" | "pack3" | "pack5" | "pack10" | "unlimited" | "premium";
-}
